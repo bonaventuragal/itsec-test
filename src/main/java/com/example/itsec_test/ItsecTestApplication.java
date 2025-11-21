9c7b12bc232c4d3cdc4ab54676328ebd1e760914
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2025-11-21 16:57:36 +0700

    feat: initial commit

---
+++ src/main/java/com/example/itsec_test/ItsecTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.itsec_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsecTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsecTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/itsec_test/ItsecTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itsec_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsecTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
